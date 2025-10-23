43e990c0165c50f537ddfdf57386d3d7b9b1db74
chokkavenkatalakshmi9-creator <chokkavenkatalakshmi9@gmail.com>
2025-10-23 17:27:22 +0530

    Restore '23/10/2025, 17:18:24 Initialize empty repo'

--- main.ts
+++
@@ -1,9 +0,0 @@
-controller.player3.onEvent(ControllerEvent.Connected, function () {
-	
-})
-info.onScore(100, function () {
-	
-})
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-	
-})
